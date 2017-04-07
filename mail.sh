a1f2cec832b00cd883583b3ac4b3bb052ab2184e
root <root@yaohua.ml>
2017-04-07 09:03:02 -0400

    Fix the issue 'Couldn't lock /sent' in vps

--- mail.sh
+++ mail.sh
@@ -1,8 +1,10 @@
 #! /bin/bash -x
+HOME=/root
 BCC=sunyaohua@aol.com
 SUBJECT='Re: [387ddd78778f] Task XX of the Eudyptula Challenge'
 RECEPIENT=little@eudyptula-challenge.org
-ATTACHMENTS='-a /root/ -a /root/ '
+#RECEPIENT=sunyaohua@gmail.com
+#ATTACHMENTS='-a /root/ -a /root/ '
 mutt -b $BCC -s "$SUBJECT" $ATTACHMENTS -- $RECEPIENT << HERE_EOF
 Hello there,
 Here's the solution, please check the attachments.
